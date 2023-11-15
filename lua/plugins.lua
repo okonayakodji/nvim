a88933674e6c2604daf288f67f9818975635ef0f
okonayakodji <okonayakodji@mail.ru>
2023-11-15 20:15:18 +0300

    Add lsp-config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,7 +1,9 @@
 vim.cmd [[packadd packer.nvim]]
 return require("packer").startup(function(use)
 	use {
-  		'nvim-telescope/telescope.nvim', 
-  		requires = { {'nvim-lua/plenary.nvim'} }
+  		"nvim-telescope/telescope.nvim", 
+  		requires = { {"nvim-lua/plenary.nvim"}
+    },
+    use "neovim/nvim-lspconfig"
 }
 end)
