621c6c8a39aff40857778295628cd43b4618e076
okonayakodji <okonayakodji@mail.ru>
2023-11-16 09:06:12 +0300

    Install treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,5 +10,6 @@ return require("packer").startup(function(use)
   use "sheerun/vim-polyglot",
   use "stevearc/oil.nvim",
   use "nvim-lua/completion-nvim",
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
 }
 end)
