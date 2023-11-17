7fd22f8f4e4fb3fe0f35846bc84ed3625be404d3
okonayakodji <okonayakodji@mail.ru>
2023-11-17 11:46:33 +0300

    Search hidden files

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,5 @@
 local keymap = vim.keymap
-keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", {})
+keymap.set("n", "<leader>ff", ":Telescope find_files find_command=rg,--hidden,--files,--ignore<CR>", {})
 keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", {})
 keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>", {})
 keymap.set("n", "<leader>ft", ":Telescope<CR>", {})
