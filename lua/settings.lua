c62e5e227519d8a34ef4fa28bea4ad3d5e01215d
okonayakodji <okonayakodji@mail.ru>
2023-11-18 09:56:51 +0300

    Diagnostic comments

--- lua/settings.lua
+++ lua/settings.lua
@@ -10,5 +10,13 @@ vim.opt.tabstop = 4
 vim.opt.termguicolors = true
 vim.opt.relativenumber = true
 vim.opt.swapfile = false
+vim.opt.wrap = true
 vim.cmd [[highlight! link SignColumn LineNr]]
 vim.cmd [[autocmd BufWritePost,FileWritePost * silent! :%s/\s\+$]]
+vim.diagnostic.config({
+  virtual_text = true 
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
