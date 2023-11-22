c0953aa320554e8393597dcca9d2dec9be78059a
okonayakodji <okonayakodji@mail.ru>
2023-11-22 16:02:57 +0300

    Add keymap for :noh

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,5 @@
 local keymap = vim.keymap
+keymap.set("n", "<C>l", ":noh", {})
 keymap.set("n", "<leader>ff", ":Telescope find_files find_command=rg,--hidden,--files,--ignore<CR>", {})
 keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", {})
 keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>", {})
